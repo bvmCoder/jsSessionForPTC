d0e751e6d5bbf34e65c776cde428cc4dea51d9ea
Dixit Patel <talkwithdixit@gmail.com>
2016-05-31 06:37:52 -0400

    Added Small Object Exercise

--- sessionPTC.js
+++ sessionPTC.js
@@ -1,95 +1,127 @@
-'use strict';
-/*
-var box = {};
-box.material = "CardBoard";
-// they both are same 
-// box["material"] = "CardBoard";
+(function() {
 
-console.dir(box); // it will print box object
+    'use strict';
+    /*
+    var box = {};
+    box.material = "CardBoard";
+    // they both are same 
+    // box["material"] = "CardBoard";
 
-var cb = box.material;
+    console.dir(box); // it will print box object
 
-console.log(cb);
+    var cb = box.material;
 
-box.material = "Titanium";
+    console.log(cb);
 
-console.log(box["material"]);
+    box.material = "Titanium";
 
-console.log(cb); // storing things by Value
+    console.log(box["material"]);
 
-
-console.log(box.size); // undefined
-
-var keyFunc = function keyFunc() {
-    return "material";
-};
-
-console.log(box[keyFunc()]);
+    console.log(cb); // storing things by Value
 
 
-var key = 'material';
+    console.log(box.size); // undefined
 
-box['key']; // undefined
+    var keyFunc = function keyFunc() {
+        return "material";
+    };
 
-console.log(box.key); // don't use dot notation with the variable
-
-
-var box = {};
-
-box['material'] = 'CardBoard';
-box[0] = 'Empty Box';
-box['^&*'] = 'Test Here';
-
-console.dir(box);
-
-*/
-
-/*
-
-DOT Notation
-strings works
-numbers doesn't work
-Quotations doesn't work
-Weired Characters doesn't work
-Expressions doesn't work
-
-Brackets Notation
-
-"strings" work
-'Weird Characters' works
-
-without the quotation mark
-variables work
-numbers work
-expressions work
-
-*/
+    console.log(box[keyFunc()]);
 
 
-// we can store anything in the object
+    var key = 'material';
 
-var box = {};
+    box['key']; // undefined
 
-box["material"] = "CardBoard";
+    console.log(box.key); // don't use dot notation with the variable
 
-box["size"] = {
-    "height": 2,
-    "width": 80
-};
 
-box[0] = 'meow';
+    var box = {};
 
-//console.log(box["size"]["height"]);
-//console.log(box['size']['width']);
+    box['material'] = 'CardBoard';
+    box[0] = 'Empty Box';
+    box['^&*'] = 'Test Here';
 
-box.area = function area() {
-    return box.size.height * box.size.width;
-};
+    console.dir(box);
 
-//console.log(box);
+    */
 
-var prop;
-for (prop in box) {
-	//console.log(prop);
-	console.log(box[prop]+ ' and its Property Key is: ' + prop);
-}
+    /*
+
+    DOT Notation
+    strings works
+    numbers doesn't work
+    Quotations doesn't work
+    Weired Characters doesn't work
+    Expressions doesn't work
+
+    Brackets Notation
+
+    "strings" work
+    'Weird Characters' works
+
+    without the quotation mark
+    variables work
+    numbers work
+    expressions work
+
+
+
+
+    // we can store anything in the object
+
+    var box = {};
+
+    box["material"] = "CardBoard";
+
+    box["size"] = {
+        "height": 2,
+        "width": 80
+    };
+
+    box[0] = 'meow';
+
+    //console.log(box["size"]["height"]);
+    //console.log(box['size']['width']);
+
+    box.area = function area() {
+        return box.size.height * box.size.width;
+    };
+
+    //console.log(box);
+
+    var prop;
+    for (prop in box) {
+    	//console.log(prop);
+    	console.log(box[prop]+ ' and its Property Key is: ' + prop);
+    }
+
+    */
+
+
+    console.log('animal Object');
+
+    var animal = {};
+
+    animal.userName = 'Mittens';
+    animal['tagline'] = 'Pet me!';
+
+
+    var noises = [];
+    animal.noises = noises;
+
+    console.log(animal);
+
+    var count = 0,
+    	key;
+
+    for(key in animal) {
+    	count++; // count = count + 1;
+    	console.log(key);
+    	if(key === 'userName') console.log(); // console.log('Hi my name is: ' + animal[key]);
+    	else if(key === 'tagline') console.log('I like to say ' + animal[key]);
+    }
+
+
+
+})();
